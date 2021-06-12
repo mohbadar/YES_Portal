43a14309605aa079a052570c27b7e9e5d16d57b1
Safiullah Ahmadzai <safiahmadzai49@gmail.com>
2021-06-12 17:44:55 +0430

    Refactor news-details

--- src/app/pages/news-opportunities/news-opportunities.module.ts
+++ src/app/pages/news-opportunities/news-opportunities.module.ts
@@ -4,7 +4,7 @@ import { NewsOpportunitiesComponent } from './news-opportunities.component';
 import { RouterModule, Routes } from '@angular/router';
 import { SuccessStoriesComponent } from './components/success-stories/success-stories.component';
 import { SharedModule } from 'src/app/shared.module';
-import { NewsDetailsComponent } from './components/news-details/news-details.component';
+import { NewsDetailsComponent } from './components/youth-news/news-details/news-details.component';
 import { SuccessStoryDetailsComponent } from './components/success-stories/success-story-details/success-story-details.component';
 import { YouthNewsComponent } from './components/youth-news/youth-news.component';
 
