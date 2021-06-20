f624922a1f8158bdc90fed6fe7bc36127de8e460
sediqa mousavi <sediqa.mousavi@gmail.com>
2021-06-20 10:35:05 +0430

    Show videos on first load

--- src/app/pages/e-learning/e-learning.component.ts
+++ src/app/pages/e-learning/e-learning.component.ts
@@ -4,56 +4,56 @@ import { TranslateService } from '@ngx-translate/core';
 import { PageService } from '../page.service';
 
 @Component({
-  selector: 'app-e-learning',
-  templateUrl: './e-learning.component.html',
-  styleUrls: ['./e-learning.component.css']
+    selector: 'app-e-learning',
+    templateUrl: './e-learning.component.html',
+    styleUrls: ['./e-learning.component.css']
 })
 export class ELearningComponent implements OnInit {
 
 
-  lang;
-  loading: boolean = false;
-  videosTypes;
-  videos;
-  firstArray;
+    lang;
+    loading: boolean = false;
+    videosTypes;
+    videos;
 
-  constructor(
-    private translate: TranslateService,
-    private pageService: PageService,
-    private sanitizar: DomSanitizer
-  ) {
+    constructor(
+        private translate: TranslateService,
+        private pageService: PageService,
+        private sanitizar: DomSanitizer
+    ) {
 
-  }
+    }
 
-  ngOnInit(): void {
-    this.lang = this.translate.currentLang;
-    this.getVideosTypes();
-  }
+    ngOnInit(): void {
+        this.lang = this.translate.currentLang;
+        this.getVideosTypes();
+    }
 
-  getVideosTypes() {
-    this.loading = true;
-    const graphqlQuery = `{
+    getVideosTypes() {
+        this.loading = true;
+        const graphqlQuery = `{
       videoTypes(locale: "${this.lang}") {
         id
          name
         slug
       }
     }`;
-    this.pageService.getData(graphqlQuery).subscribe((res: any) => {
-      this.loading = false;
-      this.videosTypes = res.data.videoTypes;
-      console.log("🚀 ~ file: e-learning.component.ts ~ line 41 ~ ELearningComponent ~ this.pageService.getData ~ videosTypes", this.videosTypes)
-    }, err => {
-      this.loading = false;
-      console.log(err);
+        this.pageService.getData(graphqlQuery).subscribe((res: any) => {
+            this.loading = false;
+            this.videosTypes = res.data.videoTypes;
+            this.getVideosByType(this.videosTypes[0].id);
+            console.log("🚀 ~ file: e-learning.component.ts ~ line 41 ~ ELearningComponent ~ this.pageService.getData ~ videosTypes", this.videosTypes)
+        }, err => {
+            this.loading = false;
+            console.log(err);
 
-    });
-  }
+        });
+    }
 
-  getVideosByType(id) {
-    console.log("🚀 ~ file: e-learning.component.ts ~ line 50 ~ ELearningComponent ~ getVideosByType ~ id", id)
-    this.loading = true;
-    const graphqlQuery = `{
+    getVideosByType(id) {
+        console.log("🚀 ~ file: e-learning.component.ts ~ line 50 ~ ELearningComponent ~ getVideosByType ~ id", id)
+        this.loading = true;
+        const graphqlQuery = `{
       videoTypes(locale: "${this.lang}" where: { id: "${id}"}) {
         id
          name
@@ -64,19 +64,16 @@ export class ELearningComponent implements OnInit {
         }
       }
     }`;
-    this.pageService.getData(graphqlQuery).subscribe((res: any) => {
-      this.loading = false;
-      this.firstArray = res.data.videoTypes;
-      this.firstArray.forEach(element => {
-        this.videos = element.videos;
-        console.log("🚀 ~ file: e-learning.component.ts ~ line 68 ~ ELearningComponent ~ this.pageService.getData ~ vid", this.videos)
-      });
-    }, err => {
-      this.loading = false;
-      console.log(err);
+        this.pageService.getData(graphqlQuery).subscribe((res: any) => {
+            console.log("🚀 ~ file: e-learning.component.ts ~ line 68 ~ ELearningComponent ~ this.pageService.getData ~ res", res)
+            this.loading = false;
+            this.videos = res.data.videoTypes[0].videos;
+        }, err => {
+            this.loading = false;
+            console.log(err);
 
-    });
+        });
 
-  }
+    }
 
 }
